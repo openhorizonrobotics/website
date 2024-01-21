28b3cad4e2ee24258f8296e341ed794d734d49a4
Aryan D <aryan.d.dalal@gmail.com>
2024-01-21 16:56:07 +0530

    Added remaining texts and images.
    - Added the 404 error text and url to homepage which can be changed later in Error404.jsx : HOME_PAGE_URL
    - Could not get the back ground image from figma

--- website/src/Error404.jsx
+++ website/src/Error404.jsx
@@ -2,17 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './Error404.css';
 
+const HOME_PAGE_URL = "";
+
 const ROBOT_404_URL = "/img/ROBO404.png";
+const ERR_404 = "img/ERROR_text.png";
+const TEXT_404 = "img/404_text.png";
+const BKGND_404 = "img/background_404.png";
 
 function Error404() {
     return (
-        <div >
+        <div
+            
+        >
             <div className='split_vert_parent'>
                 <div className='split_vert_child'>
                     <img src={ROBOT_404_URL} />
                 </div>
                 <div className='split_vert_child_center_align'>
-                    <div>404 message goes here</div>
+                    <div>
+                        <img src={ERR_404} />
+                        <img src={TEXT_404} />
+                        <p>This robot cannot localise itself.
+Did you load in the correct ROS package?</p>
+                        <p>Go back to <a href={HOME_PAGE_URL}>Home page</a></p>
+                    </div>
                 </div>
             </div>
 
