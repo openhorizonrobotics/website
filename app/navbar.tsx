2af83d5c7e94cc3946d549fcc50931f4767c1613
thornxyz <subhrajyotighosh972@gmail.com>
2024-05-27 14:51:46 +0530

    adding files

---
+++ app/navbar.tsx
@@ -0,0 +1,28 @@
+"use client"
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import React from 'react'
+
+function Navbar() {
+    const pathname = usePathname();
+    return (
+        <div className='flex justify-center my-6'>
+            <ul>
+                <Link className={`link ${pathname === '/' ? 'text-red-500 mx-4' : 'mx-4'}`} href="/">
+                    Home
+                </Link>
+                <Link className={`link ${pathname === '/aboutus' ? 'text-red-500 mx-4' : 'mx-4'}`} href="/aboutus">
+                    About Us
+                </Link>
+                <Link className={`link ${pathname === '/blog' ? 'text-red-500 mx-4' : 'mx-4'}`} href="/blog">
+                    Blog
+                </Link>
+                <Link className={`link ${pathname === '/contactus' ? 'text-red-500 mx-4' : 'mx-4'}`} href="/contactus">
+                    Contact Us
+                </Link>
+            </ul>
+        </div>
+    )
+}
+
+export default Navbar
