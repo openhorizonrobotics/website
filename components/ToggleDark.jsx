353eec96c72400668d92c1fc85e97c180f2208eb
dragoon4890 <harshsks123@gmail.com>
2024-05-28 02:18:33 +0530

    fixed the toggle breaking due to server side , client side mismatch

--- components/ToggleDark.jsx
+++ components/ToggleDark.jsx
@@ -1,16 +1,27 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import { useTheme } from "next-themes";
 import styles from "./ToggleDark.module.css";
 
 const DarkModeToggle = () => {
     const { theme, setTheme } = useTheme();
+    const [isMounted, setIsMounted] = useState(false);
+
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
+
+    const getGreenPosition = () => {
+        if (!isMounted) return null; // Don't apply styles on server-side rendering
+            return theme === "light" ? { right: "2px" } : { left: "2px" };
+        }
 
     return (
         <div className={styles.container}>
             <div className={styles.icon} onClick={() => setTheme("dark")}>🌙</div>
             <div className={styles.icon} onClick={() => setTheme("light")}>🔆</div>
-            <div className={styles.green} style={theme === "light" ? { right: "2px" } : { left: "2px" }}></div>
+            <div className={styles.green} style={getGreenPosition()} />
         </div>
     );
 };
